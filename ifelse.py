b6ad1e38eac0bc1673236230f5899093eaa1d4ed
Uva-nana <uvananaj1l@gmail.com>
2026-04-06 14:55:49 +0530

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,18 @@
+#ifelsestatement
+num = 50
+nums = 30
+
+if num>nums:
+    print(f"The {num} is greater than 30")
+
+
+if num>=50:
+    print(f"The num {num} is greater/or equal to 50")
+
+user_logged_in = True
+
+if user_logged_in:
+    print("The user logged in successfully")
+
+
+#Python evalutes many types of values as True or False in an if statement
